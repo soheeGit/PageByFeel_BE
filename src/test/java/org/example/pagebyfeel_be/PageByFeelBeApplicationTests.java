802883efe8803a6070a46c0c4094074a00f034d7
soheeGit <63wlsthgml@gmail.com>
2025-11-20 15:54:41 +0900

    init: 초기세팅

---
+++ src/test/java/org/example/pagebyfeel_be/PageByFeelBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pagebyfeel_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageByFeelBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
